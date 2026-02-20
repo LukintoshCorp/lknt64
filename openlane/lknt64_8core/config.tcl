1f4a65ad4167fb702b4a832821636d51986f60d4
Lukintosh <rockgsamesp@gmail.com>
2026-02-20 11:55:29 -0300

    Add configuration settings for lknt64_8core

--- openlane/lknt64_8core/config.tcl
+++ openlane/lknt64_8core/config.tcl
@@ -1,1 +1,11 @@
+set ::env(DESIGN_NAME) lknt64_8core
+set ::env(VERILOG_FILES) "$::env(DESIGN_DIR)/../../verilog/top.v"
 
+set ::env(CLOCK_PORT) "clk"
+set ::env(RESET_PORT) "reset"
+set ::env(CLOCK_PERIOD) "25" ;# 40MHz
+
+set ::env(FP_CORE_UTIL) "35"
+set ::env(PL_TARGET_DENSITY) "0.45"
+set ::env(ROUTING_CORES) 4
+set ::env(DIODE_INSERTION_STRATEGY) 4
