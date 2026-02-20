644954e90993f7c6dec5e34b1d2179f99dc3e77b
Lukintosh <rockgsamesp@gmail.com>
2026-02-20 11:55:57 -0300

    Add user_project_wrapper module with CPU instantiation

--- caravel/verilog/rtl/user_project_wrapper.v
+++ caravel/verilog/rtl/user_project_wrapper.v
@@ -1,1 +1,16 @@
 
+module user_project_wrapper (
+    input wb_clk_i,
+    input wb_rst_i,
+    inout [`MPRJ_IO_PADS-1:0] io
+);
+
+    wire clk = wb_clk_i;
+    wire reset = wb_rst_i;
+
+    lknt64_8core cpu(
+        .clk(clk),
+        .reset(reset)
+    );
+
+endmodule
